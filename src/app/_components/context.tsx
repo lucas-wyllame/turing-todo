43c46eb304822ecf9ebabfa0774f21db0b79e4e0
lucas-wyllame <lucaswyllame12@gmail.com>
2026-04-09 14:19:19 -0300

    first commit

---
+++ src/app/_components/context.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export function HomeContent() {
+  return (
+    <>
+      <div className="flex bg-red-500 w-screen h-22 justify items-center justify-end px-10">
+        <button className="text-center rounded-lg border-none bg-[#302E48] px-5  h-14 cursor-pointer">+ Novo Card</button>
+      </div>
+      <main>
+        <div>
+          <p>
+            <span></span>
+          </p>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
+}
